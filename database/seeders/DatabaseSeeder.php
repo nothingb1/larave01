67a3ab715de915f18a628a44d1b71e418ae28cc6
nothingb1 <linhthuycp01@gmail.com>
2021-01-30 10:48:34 +0700

    first commit

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // \App\Models\User::factory(10)->create();
+            $this->call([
+            TheLoaiTableSeeder::class,
+            LoaiTinTableSeeder::class,
+            UserTableSeeder::class,
+            TinTableSeeder::class,
+            CommentTableSeeder::class
+
+
+        ]);
+    }
+}
